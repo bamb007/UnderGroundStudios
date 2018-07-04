a29c82805b3241fe13109548ea916d7bad1bfd19
Casper <casper307@godmail.dk>
2018-07-04 22:07:25 +0200

    Rotation script done and added Orbit script done and added Status effect in progress Detection range in progress

---
+++ 001/Assets/Scripts/RangeDetection.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangeDetection : MonoBehaviour
+{
+    [Header("Size")]
+
+    [SerializeField]
+    private float radius;
+
+    [Header("Offset")]
+
+    [SerializeField]
+    private float xOffset;
+
+    [SerializeField]
+    private float yOffset;
+
+    [SerializeField]
+    private float zOffset;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
