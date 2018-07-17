26938ef5b4db4018b432f67daf2ee2ffb9bb888c
gelor <g86318010@hotmail.com>
2018-07-17 19:46:12 +0200

    player

---
+++ 001/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+
+    // Use this for initialization
+
+    public GameObject currentCheckpoint;
+    public PlayerController player;
+    public GameObject deathParticle;
+    public GameObject respawnParticle;
+    public float respawnDelay;
+
+    public LevelManager()
+    {
+    }
+
+    void Start()
+    {
+        player = FindObjectOfType<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void RespawnPlayer()
+    {
+        StartCoroutine("RespawnPlayerCO");
+    }
+
+    public IEnumerator RespawnPlayerCO()
+    {
+        Instantiate(deathParticle, player.transform.position, player.transform.rotation);
+        player.enabled = false;
+        player.GetComponent<Renderer>().enabled = false;
+        Debug.Log("Player Respawn");
+        yield return new WaitForSeconds(respawnDelay);
+        player.transform.position = currentCheckpoint.transform.position;
+        player.enabled = true;
+        player.GetComponent<Renderer>().enabled = true;
+        Instantiate(respawnParticle, currentCheckpoint.transform.position, currentCheckpoint.transform.rotation);
+    }
+}
---
+++ 001/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public Animator anim;
+    [SerializeField]
+    public float speed = 3f;
+    public float velX;
+    public float velY;
+    bool facingRight = true;
+    private Rigidbody2D rb2d;
+    public float jumpHeight;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+    private bool grounded;
+    private bool doubleJumped;
+
+	// Use this for initialization
+	void Start () {
+
+        anim = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+
+    private void FixedUpdate()
+    {
+        bool temp = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
+        if (!grounded && temp)
+        {
+            anim.SetInteger("State", 0);
+
+        }
+
+        grounded = temp;
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        velX = Input.GetAxisRaw("Horizontal");
+        velY = rb2d.velocity.y;
+        rb2d.velocity = new Vector2(velX * speed, velY);
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            anim.SetInteger("State", 1);
+        }
+        else if (Input.GetKeyUp(KeyCode.A))
+        {
+            anim.SetInteger("State", 0);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            anim.SetInteger("State", 1);
+        }
+        else if (Input.GetKeyUp(KeyCode.D))
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            anim.SetInteger("State", 2);
+        }
+        else if (Input.GetKeyUp(KeyCode.R))
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        if (grounded)
+        {
+            doubleJumped = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            Jump();
+            anim.SetInteger("State", 3);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && !doubleJumped && !grounded)
+        {
+            Jump();
+            doubleJumped = true;
+            anim.SetInteger("State", 0);
+            anim.Update(0);
+            anim.SetInteger("State", 3);
+        
+        }
+
+        Attack();
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 localScale = transform.localScale;
+        if (velX > 0)
+        {
+            facingRight = true;
+        }
+        else if (velX < 0)
+        {
+            facingRight = false;
+        }
+        if (((facingRight) && (localScale.x < 0)) || ((!facingRight) && (localScale.x > 0)))
+        {
+            localScale.x *= -1;
+        }
+
+        transform.localScale = localScale;
+    }
+
+    public void Jump()
+    {
+        rb2d.AddForce(Vector2.up * jumpHeight);
+    }
+
+    public void Attack()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            anim.SetInteger("State", 4);
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            anim.SetInteger("State", 0);
+        }
+    }
+}
