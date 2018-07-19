745b3531a02e0652a426a858808b9b72d98e04fa
Casper <casper307@godmail.dk>
2018-07-19 21:26:02 +0200

    AI move test  and script bug testing

---
+++ 001/Assets/Scripts/EnemyStats.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStats : MonoBehaviour {
+
+    [SerializeField]
+    private int health;
+
+    [SerializeField]
+    private int resistence;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ 001/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    [SerializeField]
+    private float movementSpeed;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Move();
+	}
+
+    private void Move()
+    {
+       if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(1 * movementSpeed, 0, 0);
+        }
+       else if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-1 * movementSpeed, 0, 0);
+        }
+       else if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0, 1 * movementSpeed, 0);
+        }
+       else if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += new Vector3(0, -1 * movementSpeed, 0);
+        }
+    }
+}
+
