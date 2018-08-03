458872a08e1c1fb41918bb926954933f045bf38a
gelor <g86318010@hotmail.com>
2018-08-03 12:48:38 +0200

    stuf

---
+++ 001/Assets/Scripts/Bullet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+    public float speed;
+    private Rigidbody2D rb2d;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rb2d.velocity = new Vector2(speed, rb2d.velocity.y);
+        
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(gameObject);
+    }
+}
