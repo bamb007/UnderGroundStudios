57cc69bf9fdff27928e1ea1e95e45212223e5608
Casper <casper307@godmail.dk>
2018-08-07 20:34:49 +0200

    Projectile script changed to work with both enemy and player FireflyAI can now be spawned and find player Prototype loottable added

---
+++ 001/Assets/Scripts/LootTable/DropItemGameObject.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// When we're inheriting we have to insert GameObject as a type to GenericLootDropItem
+/// </summary>
+[System.Serializable]
+public class DropItemGameObject : DropItem<GameObject> { }
---
+++ 001/Assets/Scripts/LootTable/DropTableGameObject.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// When inheriting first we have to insert GenericLootDropItemGameObject instead of T and a GameObject instead of U
+/// </summary>
+[System.Serializable]
+public class DropTableGameObject : LootTable<DropItemGameObject, GameObject> { }
