435b8c2ca3b1250fb3e3316718f8ed478750effd
gelor <g86318010@hotmail.com>
2018-08-21 14:32:06 +0200

    Timer

---
+++ 001/Assets/Scripts/Timer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    private int timeSinsTheBeginning = 0;
+
+    [SerializeField]
+    private Text timerText;
+	
+	void Start () {
+        StartCoroutine(IncreasTime());
+	}
+	
+	
+	void Update () {
+		
+	}
+
+    IEnumerator IncreasTime()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            timeSinsTheBeginning++;
+
+            timerText.text = "Time: " + timeSinsTheBeginning;
+        }
+    }
+}
---
+++ 001/Assets/Scripts/UI/Splash_Screen.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Splash_Screen : MonoBehaviour {
+
+    [SerializeField]
+    private float time = 2;
+    public RawImage splashImage;
+    public int loadLevel;
+
+	// Use this for initialization
+	IEnumerator Start()
+    {
+        //StartCoroutine(ToMainMenu());
+
+        splashImage.canvasRenderer.SetAlpha(0.0f);
+
+        FadIn();
+        yield return new WaitForSeconds(2.5f);
+        FadOut();
+        yield return new WaitForSeconds(2.5f);
+        SceneManager.LoadScene(loadLevel);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    /*
+    IEnumerator ToMainMenu()
+    {
+        yield return new WaitForSeconds(time);
+        SceneManager.LoadScene(1);
+    }
+    */
+    void FadIn()
+    {
+        splashImage.CrossFadeAlpha(1.0f, 1.5f, false);
+    }
+
+    void FadOut()
+    {
+        splashImage.CrossFadeAlpha(0.0f, 2.5f, false);
+    }
+}
