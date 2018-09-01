157ef0d6f695ce01e159ca6f4fe20d872ea6e9a8
gelor <g86318010@hotmail.com>
2018-09-02 00:31:19 +0200

    dd

---
+++ 001/Assets/Scripts/UI/WeaponUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeaponUI : MonoBehaviour {
+
+    [SerializeField]
+    private Image weapon1;
+
+    [SerializeField]
+    private Image aK47;
+
+    public void HighLightWeapon()
+    {
+
+    }
+}
