c82480b67ad59a31d17cec8412b59add3767b6c0
Casper <casper307@godmail.dk>
2018-09-24 19:44:43 +0200

    Changed how the AI script works (split them up)

---
+++ 001/Assets/Scripts/Enemies/AIAttack.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(EnemyStats))]
+[RequireComponent(typeof(ProjectileStats))]
+public class AIAttack : MonoBehaviour 
+{
+	#region Fields
+
+	[Header("DEBUG")]
+	[SerializeField] private bool debugmode;
+
+    private ProjectileStats proStats;
+    private EnemyStats stats;
+
+    private GameObject targetToShot;
+
+    #endregion
+
+    #region Awake / Start / Update / FixedUpdate / LateUpdate
+    // Use this for initialization before game start
+    void Awake()
+	{
+        proStats = GetComponent<ProjectileStats>();
+        stats = GetComponent<EnemyStats>();
+        targetToShot = GameObject.FindGameObjectWithTag("Player");
+    }
+
+	// Use this for initialization
+	void Start() 
+	{
+		
+	}
+	
+	// FixedUpdate is called just before physic update
+	void FixedUpdate()
+	{
+		
+	}
+
+	// LateUpdate is called after FixedUpdate and Update
+	void LateUpdate()
+	{
+		
+	}
+
+	// Update is called once per frame
+	void Update() 
+	{
+		
+	}
+    #endregion
+
+    #region Functions
+
+    public void Attack()
+    {
+        var clone = Instantiate(proStats.projectileAttack, new Vector3(transform.position.x, transform.position.y, 0), Quaternion.identity);
+        clone.projectileSpeed = proStats.Speed;
+        clone.destroyTime = proStats.destroyTime;
+        clone.damage = stats.damage;
+        clone.target = targetToShot;
+        clone.enemyProjectile = proStats.enemyProjectile;
+        clone.playerProjectile = proStats.playerProjectile;
+        clone.canCollideWithWall = proStats.canCollideWithWall;
+        clone.ground = proStats.ground;
+    }
+
+    #endregion
+}
---
+++ 001/Assets/Scripts/Enemies/DetectionRanges.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectionRanges : MonoBehaviour 
+{
+	#region Fields
+
+	[Header("DEBUG")]
+	[SerializeField] private bool debugmode;
+
+    [SerializeField]
+    private float detectionRange, attackRange, saveRange;
+
+    [SerializeField]
+    private bool detectionRangeActive, attackRangeActive, saveRangeActive;
+
+    //Used to find player and get range distance
+    private GameObject player;
+
+    #endregion
+
+    #region Awake / Start / Update / FixedUpdate / LateUpdate
+    // Use this for initialization before game start
+    void Awake()
+	{
+		player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+	// Use this for initialization
+	void Start() 
+	{
+		
+	}
+	
+	// FixedUpdate is called just before physic update
+	void FixedUpdate()
+	{
+		
+	}
+
+	// LateUpdate is called after FixedUpdate and Update
+	void LateUpdate()
+	{
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player == null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player"); ;
+        }
+
+        if (Vector2.Distance(player.transform.position, transform.position) <= saveRange && saveRangeActive == false)
+        {
+            saveRangeActive = true;
+        }
+        else if (Vector2.Distance(player.transform.position, transform.position) > saveRange && saveRangeActive == true)
+        {
+            saveRangeActive = false;
+        }
+
+        if (Vector3.Distance(player.transform.position, transform.position) <= detectionRange && detectionRangeActive == false)
+        {
+            detectionRangeActive = true;
+        }
+        else if (Vector3.Distance(player.transform.position, transform.position) > detectionRange && detectionRangeActive == true)
+        {
+
+        }
+
+        if (Vector3.Distance(player.transform.position, transform.position) <= attackRange && attackRangeActive == false)
+        {
+            attackRangeActive = true;
+        }
+        else if (Vector3.Distance(player.transform.position, transform.position) > attackRange && attackRangeActive == true)
+        {
+            attackRangeActive = false;
+        }
+    }
+	#endregion
+
+	#region Functions
+
+	#endregion
+}
